15570049705969e0002ad324fc6528fef7ed8017
Egor Malko <e.malko@bk.ru>
2022-11-28 17:54:46 +0300

    Add Entity

---
+++ src/main/java/ru/sirius/natayarik/ft/entity/Category.java
@@ -0,0 +1,59 @@
+package ru.sirius.natayarik.ft.entity;
+
+import ru.sirius.natayarik.ft.data.Type;
+
+import javax.persistence.*;
+
+/**
+ * @author Egor Malko
+ */
+@Entity
+@Table(name = "categories")
+@SequenceGenerator(allocationSize = 1, name = "category_seq", sequenceName = "category_seq")
+public class Category {
+   @Id
+   @GeneratedValue(generator = "category_seq")
+   @Column(name = "id")
+   private long id;
+
+   @ManyToOne
+   @JoinColumn(name = "user_id")
+   private User user;
+   @Column(name = "name")
+   private String name;
+   @Enumerated(value = EnumType.STRING)
+   @Column(name = "type")
+   private Type type;
+
+   public long getId() {
+      return id;
+   }
+
+   public void setId(long id) {
+      this.id = id;
+   }
+
+   public User getUser() {
+      return user;
+   }
+
+   public void setUser(User user) {
+      this.user = user;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public Type getType() {
+      return type;
+   }
+
+   public void setType(Type type) {
+      this.type = type;
+   }
+}
